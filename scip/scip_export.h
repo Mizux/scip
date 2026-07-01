69032e62d112f82482f0f6f2f74029b01839a08b
Corentin Le Molgat <corentinl@google.com>
2026-07-01 10:23:37 +0200

    add_scip_scip_export.h

---
+++ scip/scip_export.h
@@ -0,0 +1,45 @@
+
+#ifndef SCIP_EXPORT_H
+#define SCIP_EXPORT_H
+
+#ifdef SCIP_STATIC_DEFINE
+#  define SCIP_EXPORT
+#  define SCIP_NO_EXPORT
+#  define SCIP_DEPRECATED
+#else
+#  ifndef SCIP_EXPORT
+#    ifdef libscip_EXPORTS
+        /* We are building this library */
+#      define SCIP_EXPORT __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define SCIP_EXPORT __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef SCIP_NO_EXPORT
+#    define SCIP_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+
+#  ifndef SCIP_DEPRECATED
+#    define SCIP_DEPRECATED __attribute__ ((__deprecated__))
+#  endif
+
+#endif
+
+#ifndef SCIP_DEPRECATED_EXPORT
+#  define SCIP_DEPRECATED_EXPORT SCIP_EXPORT SCIP_DEPRECATED
+#endif
+
+#ifndef SCIP_DEPRECATED_NO_EXPORT
+#  define SCIP_DEPRECATED_NO_EXPORT SCIP_NO_EXPORT SCIP_DEPRECATED
+#endif
+
+/* NOLINTNEXTLINE(readability-avoid-unconditional-preprocessor-if) */
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef SCIP_NO_DEPRECATED
+#    define SCIP_NO_DEPRECATED
+#  endif
+#endif
+
+#endif /* SCIP_EXPORT_H */
